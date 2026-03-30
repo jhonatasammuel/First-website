0defcf0e8dca5932f429b5e490423fdde33339a3
Jhonata Samuel <jhonatasammuel@gmail.com>
2026-03-30 19:18:20 -0300

    Adicionado informações dos produtos no site

--- scripts/core.js
+++ scripts/core.js
@@ -1,3 +1,11 @@
 import runEventListeners from "./event_listener.js";
+import { generateHtml } from "./generate.js";
 
 runEventListeners();
+generateHtml();
+
+document.querySelectorAll(".js-comprar-button").forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.dataset.id);
+  });
+});
